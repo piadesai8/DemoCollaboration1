14fcd44489f83394c8b51a04c3923c5dcc75ae26
pia <piadesai88@gmail.com>
2022-06-05 22:32:47 +1200

    main menu

--- GroupProject1/GroupProject1.cpp
+++ GroupProject1/GroupProject1.cpp
@@ -9,54 +9,62 @@ using namespace std;
 
 int main()
 {
-<<<<<<< HEAD
+
 	// implement main menu - piaclass
-		int choice;
 	// login implementation version1
-	string uname, password, line;
-=======
 	//registration -piawork
-	
-	
-	// login implementation version1 
-	string uname, password,line, regUname,regPwd;
+
+	int choice;	
+	string uname, password, line, regUname, regPwd;
 	ofstream ufile;
->>>>>>> d909bb4fb828d7960ea9e742927442e8893cad7d
 	ifstream userFile;
 	bool login = false;
-	char choice;
-	userFile.open("user.txt", ios::in);
-<<<<<<< HEAD
+
+	
+	
+
 	do
 	{
-		cout << " Enter 1 for registration"
+		cout << "\n\n Enter 1 for registration"
 			<< "\n Enter 2 for Login"
 			<< "\n Enter 3 for EXIT : ";
 		cin >> choice;
 		switch (choice)
 		{
 		case 1:
-
-			break;
+			ufile.open("user.txt", ios::app);
+				cout << "\n Enter Registration Details: \n";
+				cout << " Enter Username: ";
+				cin >> regUname;
+				cin.ignore();
+				cout << " Enter Password: ";
+				cin >> regPwd;
+				ufile << regUname << " " << regPwd << endl;
+				ufile.close();
+				break;
 		case 2:
-			cout << "Enter Username: ";
-			cin >> uname;
-			cin.ignore();
-			cout << "Enter Password: ";
-			cin >> password;
-			while (getline(userFile, line))
-			{
-				if (line.find(uname) != string::npos && line.find(password) != string::npos)
-					login = true;
-			}
-			if (login)
-				cout << "\n Login Successful!!";
-			else
-				cout << "\n Wrong username or password!";
-			userFile.close();
-			break;
+			userFile.open("user.txt", ios::in);
+				cout << "\n Enter Username: ";
+				cin >> uname;
+				cin.ignore();
+				cout << " Enter Password: ";
+				cin >> password;
+				while (getline(userFile, line))
+				{
+					if (line.find(uname) != string::npos && line.find(password) != string::npos)
+						login = true;
+				}
+
+				if (login)
+					cout << "\n Login Successful!!";
+				else
+					cout << "\n Wrong username or password!";
+
+				userFile.close();
+				break;
 		case 3:
-			exit(0);
+				cout << "\n Thank you!!\n\n";
+				exit(0);
 		}
 
 
@@ -69,40 +77,31 @@ int main()
 
 
 	return 0;
-=======
-	ufile.open("user.txt", ios::app);
-	cout << "\n Enter Registration Details: \n";
-	cout << " Enter Username: ";
-	cin >> regUname;
-	cin.ignore();
-	cout << " Enter Password: ";
-	cin >> regPwd;
-	ufile << regUname << " " << regPwd << endl;
-	ufile.close();
-	cout << "\n Do you want to login? (y/n)\n";
-	cin >> choice;
-	if (choice == 'y')
-	{
-		cout << " Enter Username: ";
-		cin >> uname;
-		cin.ignore();
-		cout << " Enter Password: ";
-		cin >> password;
-		while (getline(userFile, line))
-		{
-			if (line.find(uname) != string::npos && line.find(password) != string::npos)
-				login = true;
-		}
-		if (login)
-			cout << "\n Login Successful!!";
-		else
-			cout << "\n Wrong username or password!";
-
-		userFile.close();
-	}
-	else
-	{
-		cout << "\n\n Thank you!!";
-	}
->>>>>>> d909bb4fb828d7960ea9e742927442e8893cad7d
 }
+
+	//cout << "\n Do you want to login? (y/n)\n";
+	//cin >> choice;
+	//if (choice == 'y')
+	//{
+	//	cout << " Enter Username: ";
+	//	cin >> uname;
+	//	cin.ignore();
+	//	cout << " Enter Password: ";
+	//	cin >> password;
+	//	while (getline(userFile, line))
+	//	{
+	//		if (line.find(uname) != string::npos && line.find(password) != string::npos)
+	//			login = true;
+	//	}
+	//	if (login)
+	//		cout << "\n Login Successful!!";
+	//	else
+	//		cout << "\n Wrong username or password!";
+
+	//	userFile.close();
+	//}
+	//else
+	//{
+		
+
+
