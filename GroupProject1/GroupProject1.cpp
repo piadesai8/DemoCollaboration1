3c98d6524c8918354f36fb488772662c22577390
pia <piadesai88@gmail.com>
2022-06-05 21:54:25 +1200

    main menu

--- GroupProject1/GroupProject1.cpp
+++ GroupProject1/GroupProject1.cpp
@@ -9,26 +9,53 @@ using namespace std;
 
 int main()
 {
+	// implement main menu - piaclass
+		int choice;
 	// login implementation version1
-	string uname, password,line;
+	string uname, password, line;
 	ifstream userFile;
 	bool login = false;
 	userFile.open("user.txt", ios::in);
-
-
-	cout << "Enter Username: ";
-	cin >> uname;
-	cin.ignore();
-	cout << "Enter Password: ";
-	cin >> password;
-	while (getline(userFile, line))
+	do
 	{
-		if (line.find(uname) != string::npos && line.find(password) != string::npos)
-			login = true;		
-	}
-	if (login)
-		cout << "\n Login Successful!!";
-	else
-		cout << "\n Wrong username or password!";
-	userFile.close();
+		cout << " Enter 1 for registration"
+			<< "\n Enter 2 for Login"
+			<< "\n Enter 3 for EXIT : ";
+		cin >> choice;
+		switch (choice)
+		{
+		case 1:
+
+			break;
+		case 2:
+			cout << "Enter Username: ";
+			cin >> uname;
+			cin.ignore();
+			cout << "Enter Password: ";
+			cin >> password;
+			while (getline(userFile, line))
+			{
+				if (line.find(uname) != string::npos && line.find(password) != string::npos)
+					login = true;
+			}
+			if (login)
+				cout << "\n Login Successful!!";
+			else
+				cout << "\n Wrong username or password!";
+			userFile.close();
+			break;
+		case 3:
+			exit(0);
+		}
+
+
+
+	} while (choice != 3);
+
+
+	// randome feature code
+	//random feature code for testing 
+
+
+	return 0;
 }
